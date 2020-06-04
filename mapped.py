16f275ec3768edc49744004d8416fcfa455b19bb
Ben Neilsen <38196518+bneils@users.noreply.github.com>
2020-06-04 15:37:05 -0400

    Added progressbar, image partitioning, a quiet flag, and minor optimizations
    Progressbar uses tqdm
    The image partitioning utilizes numpy for speed
    The quiet flag replaces the tqdm function with a dummy one
    Minor fixes make build more stable

--- mapped.py
+++ mapped.py
@@ -7,93 +7,122 @@ from colormath.color_objects import LabColor, sRGBColor
 from colormath.color_conversions import convert_color
 from PIL import Image
 from nbt.nbt import *
-from numpy import array, argmin
+import numpy as np
 from argparse import ArgumentParser
 from json import load
+
+from tqdm import tqdm
 
 import os, sys
 
 with open('lab_colors.json') as f:
-    lab_colors = load(f)
+	lab_colors = load(f)
 
 
-def imagenbt(image):
-    """Creates a generator that yields NBT map(s) from an image"""
-    # Extract frame(s) from image
-    if image.is_animated:
-        frames = []
-        for i in range(image.n_frames):
-            image.seek(i)
-            frames.append(image.copy())
-    else:
-        frames = [image]
+def partition_image(image, box_dim=(2, 2)):
+	"""Takes in a (x, y) box count, which both default to 2"""
+	
+	x_width, y_width = box_dim
+	
+	# Convert to a size that would fit in (x_width,y_width) maps
+	image = image.convert("RGBA").resize((x_width * 128, y_width * 128))
 
-    # Loop through frame(s)
-    for frame in frames:
-        # Minecraft maps must fit in a 128x128 pixel area
-        frame = frame.resize((128, 128), Image.LANCZOS)
+	# Put pixels into rows
+	pixels = np.asarray(image)
+	
+	return [
+		Image.fromarray(pixels[y:y + 128, x:x + 128]) 
+		for y in range(0, image.height, 128)
+		for x in range(0, image.width, 128)
+	]
 
-        if frame.mode != 'RGBA':
-            frame = frame.convert('RGBA')
-        alpha_channel = frame.getchannel(3).getdata()
-        frame = frame.convert('P')
-        
-        palette = array(frame.getpalette()).reshape(256, 3)
 
-        index_to_id = [argmin(delta_e(array(convert_color(sRGBColor(*rgb), LabColor).get_value_tuple()),
-                            lab_colors)) + 4    # (4 is added due to the absent transparent values at 0-3)
-                        for rgb in palette]
+def imagenbt(image, box_n=(1, 1)):
+	"""Creates a generator that yields NBT map(s) from an image"""
+	
+	# Note: Minecraft maps must fit in a 128x128 pixel area
+	
+	# Extract frame(s) from image
+	if getattr(image, "is_animated", False):
+		frames = []
+		for i in range(image.n_frames):
+			image.seek(i)
+			if box_n != (1, 1):
+				frames += partition_image(image.copy(), box_n)
+			else:
+				frames.append(image.copy().resize((128, 128)).convert('RGBA'))
+	else:
+		frames = partition_image(image, box_n) if box_n != (1, 1) else [image.resize((128, 128)).convert('RGBA')]
 
-        # Collect a list of map color ids by finding the closest color's indice 
-        mapColorIds = [index_to_id[index] if alpha else 0 for index, alpha in zip(frame.getdata(), alpha_channel)]
+	# Loop through frame(s)
+	for frame in tqdm(frames):
+		alpha_channel = frame.getchannel(3).getdata()
+		frame = frame.convert('P')
+		
+		palette = np.array(frame.getpalette()).reshape(256, 3)
 
-        # Begin constructing the NBT file at https://minecraft.gamepedia.com/Map_item_format#map_.3C.23.3E.dat_format
-        nbtfile = NBTFile()
-        nbtfile.name = "root"
+		index_to_id = [np.argmin(delta_e(np.array(convert_color(sRGBColor(*rgb), LabColor).get_value_tuple()),
+							lab_colors)) + 4	# (4 is added due to the absent transparent values at 0-3)
+						for rgb in palette]
 
-        data = TAG_Compound()
-        data.name = "data"  # only works if on a separate line
-        nbtfile.tags.append(data)
-        
-        data.tags.extend([
-            TAG_Byte(name = "scale", value = 0),
-            TAG_Byte(name = "dimension", value = 0),
-            TAG_Byte(name = "locked", value = 1),
-            TAG_Byte(name = "trackingPosition", value = 0),
-            TAG_Int(name = "xCenter", value = 0),
-            TAG_Int(name = "zCenter", value = 0),
-            TAG_Short(name = "height", value = 128),
-            TAG_Short(name = "width", value = 128)
-        ])
+		# Collect a list of map color ids by finding the closest color's indice 
+		mapColorIds = [index_to_id[index] if alpha else 0 for index, alpha in zip(frame.getdata(), alpha_channel)]
 
-        # Load image data
-        imageData = TAG_Byte_Array(name = "colors")
-        imageData.value = mapColorIds
-        data.tags.append(imageData)
+		# Begin constructing the NBT file at https://minecraft.gamepedia.com/Map_item_format#map_.3C.23.3E.dat_format
+		nbtfile = NBTFile()
+		nbtfile.name = "root"
 
-        yield nbtfile
+		data = TAG_Compound()
+		data.name = "data"  # only works if on a separate line
+		nbtfile.tags.append(data)
+		
+		data.tags.extend([
+			TAG_Byte(name = "scale", value = 0),
+			TAG_Byte(name = "dimension", value = 0),
+			TAG_Byte(name = "locked", value = 1),
+			TAG_Byte(name = "trackingPosition", value = 0),
+			TAG_Int(name = "xCenter", value = 0),
+			TAG_Int(name = "zCenter", value = 0),
+			TAG_Short(name = "height", value = 128),
+			TAG_Short(name = "width", value = 128),
+		])
+
+		# Load image data
+		imageData = TAG_Byte_Array(name = "colors")
+		imageData.value = mapColorIds
+		data.tags.append(imageData)
+
+		frame.close()
+		yield nbtfile
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('fp', metavar='path', help='the file path of the image')
-    parser.add_argument('num', metavar='map-#', type=int, help='how many maps exist in your world.')
-    parser.add_argument('-d', '--dir', metavar='dir', default='.', help="the directory to save in. Defaults to working directory.")
+	parser = ArgumentParser()
+	parser.add_argument('fp', metavar='path', help='the file path of the image')
+	parser.add_argument('num', metavar='map-#', type=int, help='how many maps exist in your world.')
+	parser.add_argument('-d', '--dir', metavar='dir', default='.', help="the directory to save in. Defaults to working directory.")
+	parser.add_argument('-x', '--x-box-n', metavar='X', default=1, type=int, help="how many horizontal maps to split the image into.")
+	parser.add_argument('-y', '--y-box-n', metavar='Y', default=1, type=int, help="how many vertical maps to split the image into.")
+	parser.add_argument('-q', '--quiet', action='store_true', default=False, help="is used to remove the progress bar")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    try:
-        image = Image.open(args.fp)
-    except FileNotFoundError as e:
-        sys.exit(e)
+	# Silence tqdm
+	if args.quiet:
+		tqdm = lambda it, *a, **k: it
 
-    try:
-        if image.is_animated:
-            for frame in imagenbt(image):
-                frame.write_file(os.path.join(args.dir, 'map_%d.dat' % args.num))
-                args.num += 1
-        else:
-            list(imagenbt(image))[0].write_file(os.path.join(args.dir, 'map_%d.dat' % args.num))
-            
-    except FileNotFoundError as e:
-        sys.exit(e)
+	try:
+		image = Image.open(args.fp)
+	except FileNotFoundError as e:
+		sys.exit(e)
+
+	try:
+		for nbtfile in imagenbt(image, (args.x_box_n, args.y_box_n)):
+			nbtfile.write_file(os.path.join(args.dir, 'map_%d.dat' % args.num))
+			args.num += 1
+	
+	except (FileNotFoundError, KeyboardInterrupt) as e:
+		image.close()
+		sys.exit(e)
+		
+	image.close()
