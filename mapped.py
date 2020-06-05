12a7fd47cfdc8556f6e4b778a6de831c99b24ac9
Ben Neilsen <38196518+bneils@users.noreply.github.com>
2020-06-04 22:33:20 -0400

    Added dithering, GREATLY improved speed, removed monkeypatches

--- mapped.py
+++ mapped.py
@@ -2,23 +2,20 @@
 # Made by Ben Neilsen
 # Created: 6/3/20
 
-from colormath.color_diff_matrix import delta_e_cie1976 as delta_e
-from colormath.color_objects import LabColor, sRGBColor
-from colormath.color_conversions import convert_color
 from PIL import Image
 from nbt.nbt import *
-import numpy as np
+from numpy import asarray
 from argparse import ArgumentParser
 from json import load
-
 from tqdm import tqdm
-
 import os, sys
 
-with open('lab_colors.json') as f:
-	lab_colors = load(f)
+# Initialize palette
+with open('rgb_map_colors.json') as f:
+	palimage = Image.new('P', (1, 208*3))
+	palimage.putpalette(load(f))
 
-
+	
 def partition_image(image, box_dim=(2, 2)):
 	"""Takes in a (x, y) box count, which both default to 2"""
 	
@@ -26,49 +23,45 @@ def partition_image(image, box_dim=(2, 2)):
 	
 	# Convert to a size that would fit in (x_width,y_width) maps
 	image = image.convert("RGBA").resize((x_width * 128, y_width * 128))
+	
+	if box_dim == (1, 1):
+		return [image]
 
 	# Put pixels into rows
-	pixels = np.asarray(image)
+	pixels = asarray(image)
 	
 	return [
 		Image.fromarray(pixels[y:y + 128, x:x + 128]) 
 		for y in range(0, image.height, 128)
 		for x in range(0, image.width, 128)
 	]
+    
 
-
-def imagenbt(image, box_n=(1, 1)):
+def imagenbt(image, alpha_threshold=128, box_n=(1, 1)):
 	"""Creates a generator that yields NBT map(s) from an image"""
 	
-	# Note: Minecraft maps must fit in a 128x128 pixel area
-	
 	# Extract frame(s) from image
-	if getattr(image, "is_animated", False):
+	if 'loop' in image.info: # This image must be a GIF
 		frames = []
 		for i in range(image.n_frames):
 			image.seek(i)
-			if box_n != (1, 1):
-				frames += partition_image(image.copy(), box_n)
-			else:
-				frames.append(image.copy().resize((128, 128)).convert('RGBA'))
+			frames += partition_image(image.copy(), box_n)
 	else:
-		frames = partition_image(image, box_n) if box_n != (1, 1) else [image.resize((128, 128)).convert('RGBA')]
+		frames = partition_image(image, box_n)
 
 	# Loop through frame(s)
 	for frame in tqdm(frames):
-		alpha_channel = frame.getchannel(3).getdata()
-		frame = frame.convert('P')
+		alpha_channel = frame.getdata(3)
 		
-		palette = np.array(frame.getpalette()).reshape(256, 3)
-
-		index_to_id = [np.argmin(delta_e(np.array(convert_color(sRGBColor(*rgb), LabColor).get_value_tuple()),
-							lab_colors)) + 4	# (4 is added due to the absent transparent values at 0-3)
-						for rgb in palette]
-
 		# Collect a list of map color ids by finding the closest color's indice 
-		mapColorIds = [index_to_id[index] if alpha else 0 for index, alpha in zip(frame.getdata(), alpha_channel)]
+		mapColorIds = frame.convert('RGB').quantize(palette=palimage, colors=208)
 
-		# Begin constructing the NBT file at https://minecraft.gamepedia.com/Map_item_format#map_.3C.23.3E.dat_format
+		# Correct the list of map color ids (since it's not perfect)
+		# ID 34 is snow or (255,255,255).
+		# Since the palette only has 208 colors (with the rest being (255,255,255), indices greater than 207 must be set to 34)
+		mapColorIds = [(index if index < 208 else 34) if alpha >= alpha_threshold else 0 for index, alpha in zip(mapColorIds.getdata(), alpha_channel)]
+		
+		# Begin constructing the NBT file described at https://minecraft.gamepedia.com/Map_item_format#map_.3C.23.3E.dat_format
 		nbtfile = NBTFile()
 		nbtfile.name = "root"
 
@@ -104,6 +97,7 @@ if __name__ == '__main__':
 	parser.add_argument('-x', '--x-box-n', metavar='X', default=1, type=int, help="how many horizontal maps to split the image into.")
 	parser.add_argument('-y', '--y-box-n', metavar='Y', default=1, type=int, help="how many vertical maps to split the image into.")
 	parser.add_argument('-q', '--quiet', action='store_true', default=False, help="is used to remove the progress bar")
+	parser.add_argument('-a', '--alpha-threshold', metavar='N', default=128, type=int, help="the minimum alpha value for a value to be considered opaque. Default is 128.")
 
 	args = parser.parse_args()
 
@@ -117,7 +111,7 @@ if __name__ == '__main__':
 		sys.exit(e)
 
 	try:
-		for nbtfile in imagenbt(image, (args.x_box_n, args.y_box_n)):
+		for nbtfile in imagenbt(image, args.alpha_threshold, (args.x_box_n, args.y_box_n)):
 			nbtfile.write_file(os.path.join(args.dir, 'map_%d.dat' % args.num))
 			args.num += 1
 	
